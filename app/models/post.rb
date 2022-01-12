d3498cbe14003c0a53b881e06de5dbc50cb1c14a
smh1988-2 <seamhurley1988@gmail.com>
2022-01-12 10:55:25 -0800

    commit tree 0cab57f21063474c259515ab97af8a51641bd3e2

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :clickbait    
+
+    def clickbait
+        if :title.to_s.include? "True" 
+        else
+            errors.add(:title, "must be clickbait")  
+        end
+    end
+    
+
+
 end
